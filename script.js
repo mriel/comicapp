8e45db1b63ff2fdb701e1838120288986517af9d
mriel <therieldesign@gmail.com>
2017-01-03 09:05:16 -0600

    adjusting javascript, taking steps to optimize it more.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var request = new XMLHttpRequest();
 request.open("GET", "https://api.shortboxed.com/comics/v1/new", true);
+var data;
 request.onload = function() {
   if (request.status >= 200 && request.status < 400) {
     console.log("success");
-    var data = JSON.parse(request.responseText);
-    document.getElementById("title").innerHTML = data.comics[0].title;
-    document.getElementById("publisher").innerHTML = data.comics[0].publisher;
-    document.getElementById("description").innerHTML = data.comics[0].description;
+    data = JSON.parse(request.responseText);
+    document.getElementById("comics").innerHTML = "<h3> " + data.comics[0].title + " </h3>" +
+                                                  "<p>" + data.comics[0].publisher + "</p>" +
+                                                  "<p>" + data.comics[0].description + "</p>";
   } else {
     console.log(request.status + " " + "Error")
   }
