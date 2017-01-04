212305de40153f5df2077a43de969c18c08d8c1f
mriel <therieldesign@gmail.com>
2017-01-03 19:58:01 -0600

    looping through comic titles and adding to hmtl

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+var comicTitle = document.getElementById("comics");
 var request = new XMLHttpRequest();
 request.open("GET", "https://api.shortboxed.com/comics/v1/new", true);
-var data;
 request.onload = function() {
   if (request.status >= 200 && request.status < 400) {
     console.log("success");
-    data = JSON.parse(request.responseText);
-    document.getElementById("comics").innerHTML = "<h3> " + data.comics[0].title + " </h3>" +
-                                                  "<p>" + data.comics[0].publisher + "</p>" +
-                                                  "<p>" + data.comics[0].description + "</p>";
+    var mydata = JSON.parse(request.responseText);
+    myRequest(mydata.comics);
   } else {
     console.log(request.status + " " + "Error")
   }
 };
 request.send();
+function myRequest(data){
+  var htmlString = "";
+  for (i = 0; i < data.length; i++){
+    htmlString += "<p class='title'>" + data[i].title + "</p>";
+  };
+  comicTitle.insertAdjacentHTML("beforeend", htmlString);
+};
