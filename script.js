887d8fee8fc8c58f5dfbf1d378658890f40b70d8
mriel <therieldesign@gmail.com>
2017-01-03 20:03:42 -0600

    looping through publisher and description

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ request.send();
 function myRequest(data){
   var htmlString = "";
   for (i = 0; i < data.length; i++){
-    htmlString += "<p class='title'>" + data[i].title + "</p>";
+    htmlString += "<h3 class='title'>" + data[i].title + "</h3>" +
+                  "<p class='publisher'>" + data[i].publisher + "</p>" +
+                  "<p class='description'>" + data[i].description + "</p>";
   };
   comicTitle.insertAdjacentHTML("beforeend", htmlString);
 };
